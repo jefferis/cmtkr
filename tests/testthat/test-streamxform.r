b9b9c368693a4a87424a31ba2f740b4f6a2949da
Gregory Jefferis <jefferis@gmail.com>
2014-08-01 17:01:31 +0100

    add round trip test

--- tests/testthat/test-streamxform.r
+++ tests/testthat/test-streamxform.r
@@ -3,6 +3,7 @@ context("transformations")
 test_that("streamxform works",{
   reg=system.file("extdata","cmtk","FCWB_JFRC2_01_warp_level-01.list", package='cmtk')
   m=matrix(rnorm(300,mean = 50), ncol=3)
-  # check that we get a matrix
   expect_is(m2<-streamxform(m, reg), "matrix")
+
+  expect_equal(streamxform(m2, c("--inverse", reg)), m, info="round trip test")
 })
