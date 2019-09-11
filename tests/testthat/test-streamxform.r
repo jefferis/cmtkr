9742ad3a36f106c3547b55005209c094aeaeaf28
Gregory Jefferis <jefferis@gmail.com>
2019-09-11 17:06:52 +0100

    update test to use new skip_* functions

--- tests/testthat/test-streamxform.r
+++ tests/testthat/test-streamxform.r
@@ -8,11 +8,13 @@ test_that("streamxform works",{
   expect_equal(streamxform(m2, c("--inverse", reg)), m, info="round trip test")
 })
 
-if(require('nat', quietly = TRUE) && isTRUE(cmtk.version()>'2.0')) {
-  test_that("compare with nat",{
-    reg=system.file("extdata","cmtk","FCWB_JFRC2_01_warp_level-01.list", package='cmtkr')
-    m=matrix(rnorm(300,mean = 50), ncol=3)
-    # check that we get a matrix
-    expect_equal(streamxform(m, reg), xform(m, reg, direction='forward'))
-  })
-}
+
+test_that("compare with nat",{
+  skip_if_not_installed('nat')
+  skip_if_not(isTRUE(nat::cmtk.version()>'2.0'))
+
+  reg=system.file("extdata","cmtk","FCWB_JFRC2_01_warp_level-01.list", package='cmtkr')
+  m=matrix(rnorm(300,mean = 50), ncol=3)
+  # check that we get a matrix
+  expect_equal(streamxform(m, reg), nat::xform(m, reg, direction='forward'))
+})
